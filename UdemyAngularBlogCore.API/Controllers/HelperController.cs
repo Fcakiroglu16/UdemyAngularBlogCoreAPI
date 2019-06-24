ec4f668db020f228f66bca78b2560e8d8147a4c0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-06-25 00:16:18 +0300

    Ders 58(İletişim formu için backend tarafında email gönderme servisi(api) oluşturma)

---
+++ UdemyAngularBlogCore.API/Controllers/HelperController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Mail;
+using UdemyAngularBlogCore.API.Models;
+
+namespace UdemyAngularBlogCore.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HelperController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendContactEmail(Contact contact)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+
+                SmtpClient smtpClient = new SmtpClient("mail.teknohub.net");
+
+                mailMessage.From = new MailAddress("fcakiroglu@teknohub.net");
+                mailMessage.To.Add("f-cakiroglu@outlook.com");
+
+                mailMessage.Subject = contact.Subject;
+                mailMessage.Body = contact.Message;
+                mailMessage.IsBodyHtml = true;
+                smtpClient.Port = 587;
+
+                smtpClient.Credentials = new System.Net.NetworkCredential("fcakiroglu@teknohub.net", "FatihFatih31");
+
+                smtpClient.Send(mailMessage);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ UdemyAngularBlogCore.API/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace UdemyAngularBlogCore.API.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
